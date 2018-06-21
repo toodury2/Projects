b36080f05451ed2dbe6b32bf8e5270d315a309e3
toodury2 <toodury2@naver.com>
2018-06-21 15:25:57 +0900

    Add files via upload

--- 2013011792/hw6.cu
+++ 2013011792/hw6.cu
@@ -37,12 +37,12 @@ __global__ void maxpool(float *input, float *output, const int input_size, const
 	__syncthreads();
  
 	if(tx == 0 && ty == 0)
-		output[by*filter_size + bx] = tmp[row][col];
+		output[bx*filter_size + by] = tmp[row][col];
 	__syncthreads();
  
-	if(output[by*filter_size + bx] < tmp[row][col])
+	if(output[bx*filter_size + by] < tmp[row][col])
 	{
-		 output[by*filter_size + bx] = tmp[row][col];
+		 output[bx*filter_size + by] = tmp[row][col];
 	}
 	__syncthreads();
 
